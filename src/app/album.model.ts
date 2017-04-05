5c3bbae400bc3151df044d96e36e8c75d9bc186c
Brendan Grubb <brendangrubb@gmail.com>
2017-04-04 22:05:43 -0700

    add Firebase

--- src/app/album.model.ts
+++ src/app/album.model.ts
@@ -1,3 +1,7 @@
 export class Album {
-  constructor(public title: string, public artist: string, public description: string, public id: number) {}
+  constructor(
+    public title: string,
+    public artist: string,
+    public description: string,
+    public id: number) {}
 }
---
+++ src/app/album.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Album } from './album.model';
+import { ALBUMS } from './mock-albums';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
+
+@Injectable()
+export class AlbumService {
+  albums: FirebaseListObservable<any[]>;
+
+  constructor(private angularFire: AngularFire) {
+    this.albums = angularFire.database.list('albums');
+  }
+
+  getAlbums() {
+    return this.albums;
+  }
+
+  getAlbumsById(albumId: number) {
+    for (var i = 0; i <= ALBUMS.length - 1; i++) {
+      if (ALBUMS[i].id === albumId) {
+        return ALBUMS[i];
+      }
+    }
+  }
+
+}
